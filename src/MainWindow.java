6924f7ee831886875e9fbdb96b3e03ca21c84c96
wind1kk <artemisia_gg@mail.ru>
2015-11-20 12:55:20 +0900

    Login and MainWindow added.

---
+++ src/MainWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by User12 on 20.11.15.
+ */
+public class MainWindow extends JFrame{
+    public MainWindow() {
+  super();
+
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
